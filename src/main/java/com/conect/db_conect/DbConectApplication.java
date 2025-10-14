b2e1ed2ae76db3ab2aeffc34740a6ce082eb7a56
PaulPerea <pc474933@gmail.com>
2025-10-14 16:21:59 -0500

    first commit

---
+++ src/main/java/com/conect/db_conect/DbConectApplication.java
@@ -0,0 +1,13 @@
+package com.conect.db_conect;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DbConectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DbConectApplication.class, args);
+	}
+
+}
